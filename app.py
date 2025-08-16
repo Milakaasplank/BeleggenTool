b234dd48a52ce19a51ebbf66cd9e9730c223c490
Milakaasplank <94849276+Milakaasplank@users.noreply.github.com>
2025-08-16 16:45:21 +0200

    Update app.py

--- app.py
+++ app.py
@@ -19,9 +19,10 @@ st.subheader("Welkom Vince!")
 st.image("image.png")
 st.subheader("Hoe werkt het?")
 st.text("Gebruik deze app om gegevens in te vullen van een aandeel. Daarna zullen automatisch financiële ratio's van het aandeel worden berekend. Onder elke ratio staat een richtlijn die uit het boek Beleggen voor Dummies komt. Je kan dit 'advies' gebruiken om betere investeringsbeslissingen te nemen.")
+st.subheader("Navigatie")
 st.text("Er is een Aandelen pagina waar je de benodigde informatie kan invullen over een aandeel. Deze informatie vind je op De Giro pagina bij elk aandeel.")
 
 # Itemize the coming updates
-st.text("Komende updates:")
+st.subheader("Komende updates:")
 st.text("- Laatste nieuwsberichten van een aandeel")
 st.text("- ETF pagina")
