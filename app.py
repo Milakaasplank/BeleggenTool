c3515a44347f7ac9c962369f76bb0f1896690e42
Milakaasplank <94849276+Milakaasplank@users.noreply.github.com>
2025-08-16 16:41:11 +0200

    add image

--- app.py
+++ app.py
@@ -16,6 +16,7 @@ import openpyxl
 # Title
 st.title("Fundamentele Analyse: Aandeel")
 st.subheader("Welkom bij de Fundamentele Analyse App Vince!")
+st.image("image.png")
 st.text("Gebruik deze app om financiële ratio's van aandelen te berekenen. Je kan de data gebruiken om betere investeringsbeslissingen te nemen.")
 st.text("Er is een Aandelen pagina waar je informatie kan invullen over een aandeel. Deze informatie vind je op De Giro pagina bij elk aandeel.")
 # Itemize the coming updates
