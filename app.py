a67038828ed069a6afcf27af4d6440eea19be06f
Milakaasplank <94849276+Milakaasplank@users.noreply.github.com>
2025-08-16 16:44:26 +0200

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ import openpyxl
 # Title
 st.title("Fundamentele Analyse: Aandeel")
 st.subheader("Welkom Vince!")
-st.image("image.png", use_column_width=True)
+st.image("image.png", use_container_width =True)
 st.subheader("Hoe werkt het?")
 st.text("Gebruik deze app om gegevens in te vullen van een aandeel. Daarna zullen automatisch financiële ratio's van het aandeel worden berekend. Onder elke ratio staat een richtlijn die uit het boek Beleggen voor Dummies komt. Je kan dit 'advies' gebruiken om betere investeringsbeslissingen te nemen.")
 st.text("Er is een Aandelen pagina waar je de benodigde informatie kan invullen over een aandeel. Deze informatie vind je op De Giro pagina bij elk aandeel.")
